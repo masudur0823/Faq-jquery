8203d13faa9855243a10810d048c742d712a0695
masudur0823 <masudur0823@gmail.com>
2022-10-30 20:38:46 +0600

    edited

---
+++ js/function.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $("#toggle1").click(function(){
+      $(".testP1").toggle();
+      $(".icon1").toggle();
+      $(".icon-minus1").toggle();
+    });
+  });
+  $(document).ready(function(){
+    $("#toggle2").click(function(){
+      $(".testP2").toggle();
+      $(".icon2").toggle();
+      $(".icon-minus2").toggle();
+    });
+  });
+  $(document).ready(function(){
+    $("#toggle3").click(function(){
+      $(".testP3").toggle();
+      $(".icon3").toggle();
+      $(".icon-minus3").toggle();
+    });
+  });
